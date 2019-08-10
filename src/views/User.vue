6c9536f93d1a5271f9764e478429bd9c6e359e63
harapeko <gmail@harapeko.dev>
2019-08-10 16:18:31 +0900

    add get user data

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,3 +3,22 @@
     <h1>This is a user page</h1>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'user',
+    data: () => {
+      return {
+        users: [],
+      }
+    },
+    created: function () {
+      const self = this;
+      axios.get('https://randomuser.me/api/?results=10')
+        .then(function (response) {
+          console.log(response);
+          self.users = response.data.results;
+        })
+    }
+  }
+</script>
